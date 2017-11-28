87e0ed7b05b36f99176352efdf6f6485ac577c0f
binhnguyennus <binhnguyenvnuhcm@gmail.com>
2017-11-29 07:31:05 +0800

    Add image bookmarking site

--- bookmarks/bookmarks/settings.py
+++ bookmarks/bookmarks/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'social.apps.django_app.default',
+    'images',
 )
 
 MIDDLEWARE_CLASSES = (
---
+++ bookmarks/images/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Image',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
+                ('title', models.CharField(max_length=200)),
+                ('slug', models.SlugField(max_length=200, blank=True)),
+                ('url', models.URLField()),
+                ('image', models.ImageField(upload_to='images/%Y/%m/%d')),
+                ('description', models.TextField(blank=True)),
+                ('created', models.DateTimeField(db_index=True, auto_now_add=True)),
+                ('user', models.ForeignKey(related_name='images_created', to=settings.AUTH_USER_MODEL)),
+                ('users_like', models.ManyToManyField(blank=True, related_name='images_liked', to=settings.AUTH_USER_MODEL)),
+            ],
+        ),
+    ]
